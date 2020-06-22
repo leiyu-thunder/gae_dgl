26edf890a381f532a4e11d8771c7b9df1d59fea9
leiyu-thunder <leiyu.thunder@gmail.com>
2020-06-22 17:13:35 +0800

    update

--- exp/_nbdev.py
+++ exp/_nbdev.py
@@ -8,14 +8,19 @@ index = {"gcn_msg": "00_model.ipynb",
          "InnerProductDecoder": "00_model.ipynb",
          "GAEN": "00_model.ipynb",
          "GAE": "00_model.ipynb",
+         "load_data": "01_utils.ipynb",
          "parse_index_file": "01_utils.ipynb",
-         "get_acc": "01_utils.ipynb"}
+         "load_graph": "01_utils.ipynb",
+         "ismember": "01_utils.ipynb",
+         "make_test_val_edges": "01_utils.ipynb",
+         "get_acc": "01_utils.ipynb",
+         "get_scores": "01_utils.ipynb"}
 
 modules = ["model.py",
            "utils.py"]
 
-doc_url = "https://fastai.github.io/exp/"
+doc_url = "https://leiyu-thunder.github.io/exp/"
 
-git_url = "https://github.com/fastai/exp/tree/master/"
+git_url = "https://github.com/leiyu-thunder/exp/tree/master/"
 
 def custom_doc_links(name): return None
